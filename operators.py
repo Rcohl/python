1ee54ad292362a3e7c3285e55b3c3df933d8189e
Rcohl <corentin.orczyk@outlook.fr>
2023-06-06 15:09:47 +0200

    cours sur les operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+#arithmétique
+a = 123 + 42
+a = 123 - 42
+a = 123 * 42
+a = 123 / 42
+
+#division entière, integer division
+a = 123 // 42
+print(a)
+
+#modulo ou reste de la division (euclidienne)
+a = 53
+#si il y a un reste, le nombre n'est pas divisible par 2, alors il n'est pas pair
+print(a % 2)
+
+a = 74
+#si il n'y a pas de reste, le nombre est divisible par 2, alors il est pair
+print(a % 2)
+
+#puissance, exponentiation
+#deux puissance trois
+a = 2 ** 3
+print(a)
