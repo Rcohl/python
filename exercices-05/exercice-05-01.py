b5f38e965cf59a6694dcb7bda8c234a36f1bd8dc
Rcohl <corentin.orczyk@outlook.fr>
2023-06-13 09:48:19 +0200

    exercice 5.1

---
+++ exercices-05/exercice-05-01.py
@@ -0,0 +1,17 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+
+def multiplication(a: float, b: float) -> float:
+    """
+    Cette fonction effectue une multiplication entre deux variables
+    les variables sont
+    a (float) qui représente la 1ère variable à multiplier
+    b (float) qui représente la 2ème variable à multiplier
+    return a * b correspond au résultat de cette multiplication """
+
+    return a * b
+
+help(multiplication)
