38d0be552e5db2c75ae01f5cd649d23402ba36bb
Rcohl <corentin.orczyk@outlook.fr>
2023-06-13 12:11:17 +0200

    cours sur les conditions

--- conditions.py
+++ conditions.py
@@ -96,7 +96,7 @@ print(False and False)
 # False  False  False     True
 
 #syntaxe alternative spécifique à Python équivalent de : age >= 12 and age <=25
-print(12 <= age <= 25)
+#print(12 <= age <= 25)
 
 #OU exclusif (xor)
 print(True ^ True)
@@ -110,3 +110,63 @@ print(False ^ False)
 # True   False  True
 # False  True   True
 # False  False  False
+
+#exo courses (opérateur OU logique)
+#affichez "je peux aller faire les courses" si j'ai au moins un moyen de paiement
+#affichez "je ne peux pas aller faire les courses" si je n'ai pas de moyen de paiement
+
+has_cash = bool(random.randint(0 , 1))
+has_cb = bool(random.randint(0 , 1))
+
+print(f'{has_cash}')
+print(f'{has_cb}')
+
+if has_cash or has_cb:
+    print("je peux aller faire les courses")
+else:
+    print("je ne peux pas aller faire les courses")
+
+#exo courses (opérateur ET logique)
+#affichez "je peux aller faire les courses" si j'ai au moins un moyen de paiement
+#affichez "je ne peux pas aller faire les courses" si je n'ai pas de moyen de paiement
+
+print(f'{has_cash}')
+print(f'{has_cb}')
+
+if not has_cash and not has_cb:
+    print("je ne peux pas aller faire les courses")
+else:
+    print("je peux aller faire les courses")
+
+#combinaison d'opérateur AND et OR
+user_level = 2
+user_score = 143
+user_credit = 0
+
+if user_level >=3 and user_score >=100 or user_credit >= 10:
+    print("le joueur peut acheter du matériel")
+else:
+    print("le joueur ne peut pas acheter du matériel")
+
+#exercice carte de réduction
+#déterminez la carte de réduction auquelle le voyageur a droit
+#-entre 0 et 11 ans inclus, le voyageur a droit à la gratuité
+#-entre 12 et 25 ans inclus, le voyageur a droit à une réduction de 50%
+#-entre 26 et 64 ans inclus, le voyageur a droit à une réduction de 10%
+#-au dela de 65 ans inclus, le voyageur a droit à une réduction de 50%
+
+age = random.randint(0 , 99)
+print(age)
+
+if (0 <= age <= 11):
+    print("le voyageur a droit à la gratuité")
+
+elif (12<= age <= 25):
+    print("le voyageur a droit à une réduction de 50%")
+
+elif (26 <= age <= 64):
+    print("le voyageur a droit à une réduction de 10%")
+
+elif (65 <= age <=99):
+    print("le voyageur a droit à une réduction de 50%")
+
