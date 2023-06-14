cee3193cc714c124408b1ad905de64457395d455
Rcohl <corentin.orczyk@outlook.fr>
2023-06-14 10:52:52 +0200

    exercice 6.9

---
+++ exercices-06/exercice-06-09.py
@@ -0,0 +1,16 @@
+# exo 6.9
+# Calculez la somme des nombres de la liste et affichez le résultat
+#
+# ATTENTION : ne pas utiliser la fonction sum()
+# ATTENTION : cet exercice nécessite l'utilisation d'une boucle `for`
+
+my_list = [2.71, 42, 123, 2, 3.14, 1.61]
+
+# réponse 6.9
+
+result = 0
+for ele in range(0 , len(my_list)):
+    result = result + my_list[ele]
+
+print(result)
+
