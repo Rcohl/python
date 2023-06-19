58b96d6fe068f5440d07bc705240efd7e65e091b
Rcohl <corentin.orczyk@outlook.fr>
2023-06-19 15:11:38 +0200

    exercice 6.13

--- exercices-06/exercice-06-13.py
+++ exercices-06/exercice-06-13.py
@@ -7,17 +7,10 @@ my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 
 # réponse 6.13
 
-age = random.randint(0 , 99)
-print(age)
+result = []
 
-if (0 <= age <= 11):
-    print("le voyageur a droit à la gratuité")
+for valeur in my_list:
+    result.append(valeur * 100)
 
-elif (12<= age <= 25):
-    print("le voyageur a droit à une réduction de 50%")
-
-elif (26 <= age <= 64):
-    print("le voyageur a droit à une réduction de 10%")
-
-elif (65 <= age <=99):
-    print("le voyageur a droit à une réduction de 50%")
+my_list = result
+print(my_list)
