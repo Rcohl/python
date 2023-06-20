e4a9b49d6a8d43a7d4784ebc018f3aaf7d27ea7c
Rcohl <corentin.orczyk@outlook.fr>
2023-06-20 14:23:54 +0200

    exercice 7.1

---
+++ exercices-07/exercice-07-01.py
@@ -0,0 +1,8 @@
+# exo 7.1
+# en utilisant une boucle for, affichez les nombres de 0 à 99 inclus
+
+# réponse 7.1
+
+# pour afficher 99 inclus cela revient à afficher 100 exclus
+for number in range(0, 100):
+    print(number)
