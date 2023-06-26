ba50480d2c6ac54a933cb8dcf22dbe8dc96bcf20
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 11:32:02 +0200

    exercice 7.14

---
+++ exercices-07/exercice-07-14.py
@@ -0,0 +1,15 @@
+# exo 7.14
+# en utilisant une boucle for, affichez tous les nombre pairs, de 1 à 100 inclus
+
+# réponse 7.14
+
+import random
+
+count = 0
+
+for i in range(101):
+    r = random.randint(1, 10)
+    if r <=2 or r >= 9:
+        count += 1
+
+print("Nombre de fois où r est pair:", count)
