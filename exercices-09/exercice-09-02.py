f072ec4f2509eae43db247afbcb8c6c82569aace
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 12:13:23 +0200

    exercice 9.2

---
+++ exercices-09/exercice-09-02.py
@@ -0,0 +1,18 @@
+# exo 9.2
+# Créez un dictionnaire nommé `my_dict` associant :
+# - une clé booléenne et un nombre entier
+# - une clé booléenne et un nombre à virgule flottante
+# - une clé numérique et une chaîne de caractères
+# - une clé alphanumérique et un booléen
+# Puis affichez le résultat avec un simple `print()`
+
+# réponse 9.2
+
+my_dict = {
+    True: 42,                   
+    False: 3.14,               
+    10: "Hello, World!",       
+    "abc123": True             
+}
+
+print(my_dict)
