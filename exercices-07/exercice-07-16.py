023c13eb17266ab7cfad275601cbe798888b3582
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 11:40:14 +0200

    exercice 7.16

---
+++ exercices-07/exercice-07-16.py
@@ -0,0 +1,10 @@
+# exo 7.16
+# en utilisant une boucle for, affichez la puissance 2 des nombres de 0 à 99 inclus
+
+# réponse 7.16
+
+import random
+
+for i in range(0,100):
+    r = random.randint(1, 10)
+    print(r * r)
