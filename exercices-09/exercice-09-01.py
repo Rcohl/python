5a8b1399ed45d2818f41696768be60c6c90bec0c
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 12:06:04 +0200

    exercice 9.1

---
+++ exercices-09/exercice-09-01.py
@@ -0,0 +1,18 @@
+# exo 9.1
+# Créez un dictionnaire nommé `my_dict` associant :
+# - une clé alphanumérique et un nombre entier
+# - une clé alphanumérique et un nombre à virgule flottante
+# - une clé alphanumérique et une chaîne de caractères
+# - une clé alphanumérique et un booléen
+# Puis affichez le résultat avec un simple `print()`
+
+# réponse 9.1
+
+my_dict = {
+    1: 42,
+    2: 3.14,
+    3: 'abc',
+    4: True,
+}
+
+print(my_dict)
