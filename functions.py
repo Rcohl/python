91a9592e1ecbe7767bce5726e92229ce40b8e21e
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 14:11:59 +0200

    cours sur les fonctions python

--- functions.py
+++ functions.py
@@ -42,3 +42,69 @@ def reverse_lookup(my_list, value):
             print(f'{i = }')
 
 reverse_lookup(my_list, 3.14)
+
+#type hinting
+def mult (a: int, b: int) -> int:
+    """cette fonction
+    a...
+    b...
+    retrun...
+    """
+
+    return a * b
+resultat = mult('abc', 5)
+print(resultat)
+
+# le nom de la fonction + ses paramètres + son type de retour = signature de la fonction
+# def mult(a: int, b: int) -> int:
+
+# copie d'une fonction comme si c'était une variable
+mult_copy = mult
+mult_copy(2, 5)
+
+# fonction de degré supérieur
+# une fonction qui accepte une fonction en paramètre
+# ou qui renvoie une fonction
+def oprateur_binaire(a, b, fonction):
+    return fonction(a, b)
+# appel de la fonction de degré supérieur
+resultat = oprateur_binaire(2, 5, mult)
+
+# stockage de fonctions dans une liste
+operations = []
+operations.append(addition)
+operations.append(mult)
+
+a = 2
+b = 5
+resultat = None
+
+for operation in operations:
+    resultat = operation(a, b)
+    print(resultat)
+
+my_list = ['foo','ipsum']
+text = 'toto'
+
+print(len(my_list))
+print(len(text))
+
+def my_len(value):
+    return 42
+
+# sauvegarde de la fonction len() originale
+len_backup = len
+# surcharge de la fontion len() originale càd remplacement de la fonction originale par une autre
+len= my_len
+
+print(len(my_list))
+print(len(text))
+
+# restauration de la fonction len() originale
+len = len_backup
+
+# pass permet d'écrire du code python syntaxiquement valide même quand on n'a pas encore le corps 
+# de la condition if ou de la boucle for
+
+if True:
+    pass
