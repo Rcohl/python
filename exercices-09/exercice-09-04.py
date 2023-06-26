93a1c006de3480993e6feae27c7ccb64908dc45d
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 12:23:17 +0200

    exercice 9.4

---
+++ exercices-09/exercice-09-04.py
@@ -0,0 +1,14 @@
+# exo 9.4
+# Affichez la valeur associée à la clé 'lorem' dans le dictionnaire
+
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 9.4
+
+value = my_dict['lorem']
+print(value)
