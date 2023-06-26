7bd94e9f803d4202cf2c2e2f3a51c4cf3629228a
Rcohl <corentin.orczyk@outlook.fr>
2023-06-26 10:15:31 +0200

    exercice 7.5

---
+++ exercices-07/exercice-07-05.py
@@ -0,0 +1,14 @@
+# exo 7.5
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est plus grand ou égal à 6
+
+import random
+
+# réponse 7.5
+
+for i in range(100):
+    r = random.randint(1, 10)
+if r >= 6:
+    print(r)
+else:
+    print("r est plus petit que 6")
