e00474d8cab42c88b9cdbc87d90de731d96d47ab
Rcohl <corentin.orczyk@outlook.fr>
2023-06-27 11:39:39 +0200

    cours sur la portée des valeurs JS

--- scopes.js
+++ scopes.js
@@ -22,4 +22,24 @@ myFunc1();
 // même si la variable myVar1 est déclarée avec le mot clé var, comme elle est déclarée à l'intérieur d'une fonction, 
 // elle n'est pas accessible depuis un scope extérieur
 // ReferenceError: myVar1 is not defined
-console.log(myVar1);
+//console.log(myVar1);
+
+// closure
+
+function multNFactory(n) {
+    return function multN(x) {
+        return x * n;
+    }
+}
+
+mult3 = multNFactory(3);
+
+for (let i = 0; i < 3; i++ ){
+    console.log(mult3(i));
+}
+
+mult54 = multNFactory(54);
+
+for (let i = 0; i < 3; i++ ){
+    console.log(mult54(i));
+}
